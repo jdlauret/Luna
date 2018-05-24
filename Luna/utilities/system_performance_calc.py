142aee797bb04ea8a0904ae523c09001273d681a
Unknown <mack.damavandi@gmail.com>
2018-05-24 16:32:20 -0600

    system_performance function written

--- Luna/utilities/system_performance_calc.py
+++ Luna/utilities/system_performance_calc.py
@@ -1,21 +1,54 @@
+import os
+from datetime import date, timedelta
 from Luna.models import DataWarehouse
 
-file = open('system_performance_calc.sql','r')
-sql = file.read()
-file.close()
+main_dir = os.getcwd()
+luna_dir = os.path.join(main_dir, 'Luna')
+utilities_dir = os.path.join(luna_dir, 'utilities')
 
-serviceNum = input("service number: ")
-startDate = input("start date: ")
-endDate = input("end date: ")
+def system_performance(servicenum, startdate, enddate):
 
-sql = sql.replace('serviceNum', serviceNum)
-sql = sql.replace('startDate', startDate)
-sql = sql.replace('endDate', endDate)
+    dw = DataWarehouse('admin')
 
-sql = sql.split(';')
+    file = open(os.path.join(utilities_dir, 'system_performance_calc.sql'),'r')
+    sql = file.read()
+    file.close()
 
-results = {'account': None, 'production': None}
+    sql = sql.split(';')
 
-results['account'] = ob.run_query('','',raw_query=sql[0])
+    if 'S-' in servicenum.upper():
+        servicenum = servicenum.upper().replace('S-','')
 
-print(sql)
+    bindvars = [{'serviceNum': servicenum}]
+
+    dw.query_results(sql[0], bindvars=bindvars[0])
+
+    results = {'account': dw.results}
+
+    if startdate < results['account'][0][7]:
+        startdatestring = results['account'][0][7].strftime('%m/%d/%Y')
+        results['startdate'] = results['account'][0][7]
+    else:
+        startdatestring = startdate.strftime('%m/%d/%Y')
+
+    if enddate.month >= date.today().month and enddate.year >= date.today().year:
+        enddatestring = (date.today().replace(day=1) - timedelta(1)).strftime('%m/%d/%Y')
+        results['enddate'] = date.today().replace(day=1) - timedelta(1)
+    else:
+        enddatestring = enddate.strftime('%m/%d/%Y')
+
+    bindvars.append({'serviceNum': servicenum,
+                 'startDate': startdatestring,
+                 'endDate': enddatestring})
+
+    dw.query_results(sql[1], bindvars=bindvars[1])
+
+    results['production'] = dw.results
+    totals = [sum(j) for j in [i for i in zip(*results['production'])][1:3]]
+    results['production'].append(['Total',
+                                  round(totals[0], 3),
+                                  round(totals[1], 3),
+                                  round(totals[1]/totals[0], 4)
+                                  ])
+
+    return results
--- Luna/utilities/system_performance_calc.sql
+++ Luna/utilities/system_performance_calc.sql
@@ -9,14 +9,19 @@ select t.service_name,
 from sfrpt.t_dm_project t
 inner join sfrpt.t_dm_contact c
     on c.contact_id = t.contract_signer
-where t.service_number = 'serviceNum'
+where t.service_number = :serviceNum
 ;select to_char(t.read_date,'Mon YYYY') "Month Year",
     t.estimated_corrected_kwh "Estimate (kWh)",
     t.actual_kwh "Actual (kWh)",
-    round(t.performance_ratio,4) "Performance" --t.actual_kwh/t.estimated_corrected_kwh
+    nvl(round(t.performance_ratio,4), round(t.actual_kwh/t.estimated_corrected_kwh,4)) "Performance" --t.actual_kwh/t.estimated_corrected_kwh
 from fleet_production.mv_fusion_weather_adjusted t
 inner join sfrpt.t_dm_project p
     on p.project_id = t.project_id
-where p.service_number = 'serviceNum'
-    and t.read_date between to_date('startDate','MM/DD/YYYY') and to_date('endDate','MM/DD/YYYY')
+where p.service_number = :serviceNum
+    and t.read_date between to_date(:startDate,'MM/DD/YYYY') and to_date(:endDate,'MM/DD/YYYY')
+    and t.lifetime_total_days = (select max(w.lifetime_total_days)
+                                 from fleet_production.mv_fusion_weather_adjusted w
+                                 where w.project_id = t.project_id
+                                    and w.month = t.month
+                                    and w.year = t.year)
 order by t.read_date
