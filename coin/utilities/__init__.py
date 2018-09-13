f7da15dce2ebd6e7d3a6dcbb470709564b1387c2
Grace <gsongk@gmail.com>
2018-09-13 10:26:32 -0600

    Fixed Error

--- coin/utilities/__init__.py
+++ coin/utilities/__init__.py
@@ -4,6 +4,6 @@ def find_badge_id(email):
     dw = DataWarehouse('admin')
     dw.query_results('''SELECT BADGE_ID
                             FROM WORKDAY.V_WORKDAY_CURRENT_V2
-                            WHERE WORK_EMAIL_ADDRESS = LOWER(:email)''',
+                            WHERE lower(WORK_EMAIL_ADDRESS) = LOWER(:email)''',
                      bindvars={'email': email})
     return dw.results[0][0]
