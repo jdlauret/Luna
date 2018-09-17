64e644e230b231e94790a11b6b125110cf00967d
Grace <gsongk@gmail.com>
2018-09-17 10:55:46 -0600

    recovered missing Files

--- coin/utilities/__init__.py
+++ coin/utilities/__init__.py
@@ -3,7 +3,7 @@ from Luna.models import DataWarehouse
 def find_badge_id(email):
     dw = DataWarehouse('admin')
     dw.query_results('''SELECT BADGE_ID
-                            FROM WORKDAY.V_WORKDAY_CURRENT
-                            WHERE LOWER(WORK_EMAIL_ADDRESS) = LOWER(:email)''',
+                            FROM WORKDAY.V_WORKDAY_CURRENT_V2
+                            WHERE WORK_EMAIL_ADDRESS = LOWER(:email)''',
                      bindvars={'email': email})
     return dw.results[0][0]
