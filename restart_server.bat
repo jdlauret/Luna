e07935bd5d9c7f34c9155c54a6b3db131e72e441
Unknown <jlauret89@gmail.com>
2018-09-26 15:29:18 -0600

    Updated runserver command
    Production server will no longer restart automatically on file changes.

--- restart_server.bat
+++ restart_server.bat
@@ -1,1 +1,1 @@
-cd C:\Luna_Production\venv\Scripts & activate & cd C:\Luna_Production & python manage.py runserver 0:80
+cd C:\Luna_Production\venv\Scripts & activate & cd C:\Luna_Production & python manage.py runserver 0:80 --noreload
