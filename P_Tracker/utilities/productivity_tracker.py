d3ecee64ba73a1782b74e8c141957a2a01509a46
Grace <gsongk@gmail.com>
2018-11-12 14:09:49 -0700

    Fixed SQL
    tracker was doubling the stats
    Changed debug to false

--- P_Tracker/utilities/productivity_tracker.py
+++ P_Tracker/utilities/productivity_tracker.py
@@ -1,14 +1,16 @@
 import os
+from decimal import Decimal
 from BI.data_warehouse.connector import Snowflake
 
 main_dir = os.getcwd()
 tracker_dir = os.path.join(main_dir, 'P_Tracker')
 utilities_dir = os.path.join(tracker_dir, 'utilities')
 
-DB = Snowflake()
-DB.set_user('MACK_DAMAVANDI')
 
 def tracker_list(badge):
+	DB = Snowflake()
+	DB.set_user('MACK_DAMAVANDI')
+
 	auth_list = {}
 	badge = str(badge)
 
@@ -19,7 +21,7 @@ def tracker_list(badge):
 		# sql = sql.split(';')
 		DB.execute_query(sql.format(badge=str(badge)))
 						 #% {'badge': str(badge)})
-		results = DB.query_results
+		results = [Decimal(0) if x is None else x for x in DB.query_results[0]]
 
 		if len(results) == 0:
 			auth_list['error'] = 'Information not found'
@@ -32,17 +34,17 @@ def tracker_list(badge):
 	finally:
 		DB.close_connection()
 
-	auth_list['badge'] = results[0][0]
-	auth_list['name'] = results[0][1]
-	auth_list['supervisor'] = results[0][2]
-	auth_list['hrs_worked'] = round((int(results[0][3])/60), 2)
-	auth_list['breaks'] = round((int(results[0][4])/60), 2)
-	auth_list['activity'] = round((int(results[0][5])/60), 2)
-	auth_list['training'] = round((int(results[0][6])/60), 2)
-	auth_list['meeting'] = round((int(results[0][7] + results[0][8])/60), 2)
-	auth_list['project'] = round((int(results[0][9])/60), 2)
-	auth_list['date1'] = results[0][10]
-	auth_list['date2'] = results[0][11]
+	auth_list['badge'] = results[0]
+	auth_list['name'] = results[1]
+	auth_list['supervisor'] = results[2]
+	auth_list['hrs_worked'] = round(results[3]/60, 2)
+	auth_list['breaks'] = round(results[4]/60, 2)
+	auth_list['activity'] = round(results[5]/60, 2)
+	auth_list['training'] = round(results[6]/60, 2)
+	auth_list['meeting'] = round(results[7] + results[8]/60, 2)
+	auth_list['project'] = round(results[9]/60, 2)
+	auth_list['date1'] = results[10]
+	auth_list['date2'] = results[11]
 
 	return auth_list
 
--- P_Tracker/utilities/productivity_tracker.sql
+++ P_Tracker/utilities/productivity_tracker.sql
@@ -1,19 +1,43 @@
-SELECT
-    BADGE,
-    NAME,
-    SUPERVISOR,
-    SUM(MIN_WORKED) AS MIN_WORKED,
-    SUM(BREAKS) AS BREAKS,
-    SUM(ACTIVITY_TIME) AS ACTIVITY_TIME,
-    SUM(TRAINING_TIME) AS TRAINING_TIME,
-    SUM(MEETING_TIME) AS MEETING_TIME,
-    SUM(HUDDLE_TIME) AS HUDDLE_TIME,
-    SUM(PROJECT_TIME) AS PROJECT_TIME,
-    TO_VARCHAR(PREVIOUS_DAY(DATEADD('d', -7,DATE_TRUNC('d',CURRENT_TIMESTAMP::TIMESTAMP_NTZ)),'SU'), 'MM/DD/YY') as REPORT_DATE1,
-    TO_VARCHAR(NEXT_DAY(DATEADD('d',-7,DATE_TRUNC('d',CURRENT_TIMESTAMP::TIMESTAMP_NTZ)),'sa'), 'MM/DD/YY') AS REPORT_DATE2
+SELECT BADGE,
+       NAME,
+       SUPERVISOR,
+       SUM(MIN_WORKED)        AS MIN_WORKED,
+       SUM(BREAKS)            AS BREAKS,
+       SUM(ACTIVITY_TIME)     AS ACTIVITY_TIME,
+       SUM(TRAINING_TIME)     AS TRAINING_TIME,
+       SUM(MEETING_TIME)      AS MEETING_TIME,
+       SUM(HUDDLE_TIME)       AS HUDDLE_TIME,
+       SUM(PROJECT_TIME)      AS PROJECT_TIME,
+       TO_VARCHAR(PREVIOUS_DAY(DATEADD('d', -7, DATE_TRUNC('d', CURRENT_TIMESTAMP :: TIMESTAMP_NTZ)), 'su'),
+                  'MM/DD/YY') AS WEEK_STARTING,
+       TO_VARCHAR(NEXT_DAY(DATEADD('d', -7, DATE_TRUNC('d', CURRENT_TIMESTAMP :: TIMESTAMP_NTZ)), 'sa'),
+                  'MM/DD/YY') AS WEEK_ENDING
 FROM VSLR.D_POST_INSTALL.V_SREC_AGENT_PRODUCTIVITY
 WHERE DAYOFWEEK(REPORT_DATE) BETWEEN 1 AND 5
-AND REPORT_DATE BETWEEN Previous_DAY(DATEADD('d',-7,DATE_TRUNC('d',CURRENT_TIMESTAMP::TIMESTAMP_NTZ)),'su')
-                AND NEXT_DAY(DATEADD('d',-7,DATE_TRUNC('d',CURRENT_TIMESTAMP::TIMESTAMP_NTZ)),'sa')
-AND BADGE = '{badge}'
-GROUP BY BADGE, NAME, SUPERVISOR
+  AND REPORT_DATE BETWEEN Previous_DAY(DATEADD('d', -7, DATE_TRUNC('d', CURRENT_TIMESTAMP :: TIMESTAMP_NTZ)), 'su')
+          AND PREVIOUS_DAY(DATE_TRUNC('d', CURRENT_TIMESTAMP :: TIMESTAMP_NTZ), 'su')
+  AND METRIC_LEVEL = 'Employee'
+  AND BADGE = '{badge}'
+GROUP BY BADGE, NAME, SUPERVISOR, WEEK_START, WEEK_ENDING
+ORDER BY NAME, WEEK_STARTING DESC
+
+-- SELECT BADGE,
+--        NAME,
+--        SUPERVISOR,
+--        SUM(MIN_WORKED)                                                                                               AS MIN_WORKED,
+--        SUM(BREAKS)                                                                                                   AS BREAKS,
+--        SUM(ACTIVITY_TIME)                                                                                            AS ACTIVITY_TIME,
+--        SUM(TRAINING_TIME)                                                                                            AS TRAINING_TIME,
+--        SUM(MEETING_TIME)                                                                                             AS MEETING_TIME,
+--        SUM(HUDDLE_TIME)                                                                                              AS HUDDLE_TIME,
+--        SUM(PROJECT_TIME)                                                                                             AS PROJECT_TIME,
+--        TO_VARCHAR(PREVIOUS_DAY(DATEADD('d', -7, DATE_TRUNC('d', CURRENT_TIMESTAMP :: TIMESTAMP_NTZ)), 'SU'),
+--                   'MM/DD/YY')                                                                                        as REPORT_DATE1,
+--        TO_VARCHAR(NEXT_DAY(DATEADD('d', -7, DATE_TRUNC('d', CURRENT_TIMESTAMP :: TIMESTAMP_NTZ)), 'sa'),
+--                   'MM/DD/YY')                                                                                        AS REPORT_DATE2
+-- FROM VSLR.D_POST_INSTALL.V_SREC_AGENT_PRODUCTIVITY
+-- WHERE DAYOFWEEK(REPORT_DATE) BETWEEN 1 AND 5
+--   AND REPORT_DATE BETWEEN Previous_DAY(DATEADD('d', -7, DATE_TRUNC('d', CURRENT_TIMESTAMP :: TIMESTAMP_NTZ)), 'su')
+--           AND NEXT_DAY(DATEADD('d', -7, DATE_TRUNC('d', CURRENT_TIMESTAMP :: TIMESTAMP_NTZ)), 'sa')
+--   AND BADGE = '{badge}'
+-- GROUP BY BADGE, NAME, SUPERVISOR
