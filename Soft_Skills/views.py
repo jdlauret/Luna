5ec6ee54e270a5a5c2b759e7c989926eee58346b
Grace <gsongk@gmail.com>
2018-12-05 08:39:53 -0700

    Added Description and tier to employee page

--- Soft_Skills/views.py
+++ Soft_Skills/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render, redirect
 
 from Soft_Skills.utilities.employee_list import employee_list
 from Soft_Skills.utilities.supervisor_list import supervisor_list
-from .models import Career_Path, Employee_List, Employee_ListForm, Agent_Skills
+from .models import Career_Path, Employee_List, Agent_Skills
 
 
 def email_check(user):
@@ -41,14 +41,65 @@ def index(request):
 @user_passes_test(email_check)
 def agent_skills_sheet(request):
     if request.user.is_authenticated:
+
+        print(request.POST)
+        selected_employee = int(request.POST['selected_employee'])
+        print(selected_employee, type(selected_employee))
+
         if request.method == 'POST':
-            selected_employee = int(request.POST.__getitem__('selected_employee'))
-            context={
-                'selected_employee' : Employee_List.objects.all().get(badge_id=selected_employee)
-            }
-            return render(request, 'agent_skill_set.html', context)
+            try:
+                skill_set = request.POST['select_team']
+                customer_relations = Career_Path.objects.filter(team='customer_relations')
+                recs_and_rebate = Career_Path.objects.filter(team='RECs_&_rebates')
+                central_scheduling = Career_Path.objects.filter(team='central_scheduling')
+                customer_solutions = Career_Path.objects.filter(team='customer_solutions')
+                customer_service = Career_Path.objects.filter(team='customer_service')
+
+                if skill_set == 'customer_relations':
+                    context={
+                        'selected_employee' : Employee_List.objects.all().get(badge_id=selected_employee),
+                        'customer_relations_inbound': customer_relations.filter(sub_team='inbound_outbound'),
+                        'customer_relations_rep_1': customer_relations.filter(sub_team='inbound_outbound', tier='rep_1'),
+                        'customer_relations_rep_2': customer_relations.filter(sub_team='inbound_outbound', tier='rep_2'),
+                        'customer_relations_rep_3': customer_relations.filter(sub_team='inbound_outbound', tier='rep_3'),
+                        'customer_relations_specialist_1': customer_relations.filter(sub_team='inbound_outbound', tier='specialist_1'),
+                        'customer_relations_specialist_2': customer_relations.filter(sub_team='inbound_outbound', tier='specialist_2'),
+                        'customer_relations_specialist_3': customer_relations.filter(sub_team='inbound_outbound', tier='specialist_3'),
+                        'customer_relations_team': customer_relations.filter(sub_team='inbound_outbound', tier='team_lead'),
+                    }
+                    return render(request, 'agent_skill_set.html', context)
+                if skill_set == 'RECs_&_rebates':
+                    context={
+                        'selected_employee' : Employee_List.objects.all().get(badge_id=selected_employee),
+                        'recs_and_rebate' : recs_and_rebate,
+                    }
+                    return render(request, 'agent_skill_set.html', context)
+                if skill_set == 'central_scheduling':
+                    context={
+                        'selected_employee' : Employee_List.objects.all().get(badge_id=selected_employee),
+                        'central_scheduling' : central_scheduling,
+                    }
+                    return render(request, 'agent_skill_set.html', context)
+                if skill_set == 'customer_solutions':
+                    context={
+                        'selected_employee' : Employee_List.objects.all().get(badge_id=selected_employee),
+                        'customer_solutions' : customer_solutions,
+                    }
+                    return render(request, 'agent_skill_set.html', context)
+                if skill_set == 'customer_service':
+                    context = {
+                        'selected_employee': Employee_List.objects.all().get(badge_id=selected_employee),
+                        'customer_service': customer_service,
+                    }
+                    return render(request, 'agent_skill_set.html', context)
+                else:
+                    return HttpResponseRedirect('/Soft_Skills/')
+            except:
+                context={
+                    'selected_employee' : Employee_List.objects.all().get(badge_id=selected_employee),
+                }
+                return render(request, 'agent_skill_set.html', context)
         else:
             return HttpResponseRedirect('/Soft_Skills/')
     else:
-        print('User is not authenticated')
         return HttpResponseRedirect('/Luna')
